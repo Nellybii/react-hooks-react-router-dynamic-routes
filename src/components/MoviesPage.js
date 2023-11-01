1c18b9dbae441b9c88b55beca7186ff37421dc4b
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-11-01 12:27:28 +0300

    dynamic routes

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MoviesShow from "./MoviesShow";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+
+      {/* Adding code to show a message to the user to select a movie if they haven't yet */}
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+
+      <Route path={`${match.url}/:movieId`}>
+        <MoviesShow movies={movies} />
+      </Route>
     </div>
   );
 }
+
 export default MoviesPage;
